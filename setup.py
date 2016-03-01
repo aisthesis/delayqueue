44db94333ed20c3346134d71958d585825437d10
Marshall Farrier <info@marshallfarrier.com>
2016-02-29 16:09:28 -0800

    Fix '__all__' to allow import * correct behavior.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 MAJOR   = 0
 MINOR   = 0
-MICRO   = 2
+MICRO   = 3
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
